b9b2b9050396899774bf1d502aacd10d931e1506
SalimAbdala27 <salimabdala27@hotmail.com>
2021-11-15 23:02:08 +0000

    next config working + left header with state

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,21 @@
+import Image from "next/image";
 const Header = () => {
   return (
-    <div>
-      <h1>
-        I am a Header 
-      </h1>
+  <div>
+    <div className="flex justify-between max-w-6xl bg-white">
+      {/* left */}
+      <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
+      </div>
+      <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+      <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain"/>
+      </div>
+      {/* middle  */}
+
+      {/* right */}
     </div>
-  )
+  </div>
+  ) 
 }
 
 export default Header
