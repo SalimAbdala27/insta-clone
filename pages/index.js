61be5f6dc09a48960f93a1a571e2a892f54d0424
SalimAbdala27 <salimabdala27@hotmail.com>
2021-11-26 11:54:32 +0000

    js to jsx files swapped

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from "../components/Header"
 import Feed from "../components/Feed"
 
 export default function Home() {
